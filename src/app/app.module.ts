46d07501fd5e4b837915c396cf8f898bea4393f9
Abu1234 <37133919+Abu1234@users.noreply.github.com>
2021-11-26 10:26:18 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { HomeComponent } from './home/home.component';
+
+import { MaterialComponent } from './material/material.component';
+
+const ROUTES: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'material', component: MaterialComponent },
+
+  { path: '**', redirectTo: 'home' },
+];
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,HomeComponent,MaterialComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]

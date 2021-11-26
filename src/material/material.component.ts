ca8e2b037ed29c97f3a789b3ff1444bbdcec0b22
Abu1234 <37133919+Abu1234@users.noreply.github.com>
2021-11-26 10:34:11 +0530

    Update material.component.ts

--- src/material/material.component.ts
+++ src/material/material.component.ts
@@ -1,1 +1,19 @@
 
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+
+@Component({
+  selector: 'material',
+  templateUrl: './material.component.html',
+  styleUrls: ['./material.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+
+
+}

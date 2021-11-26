02a619bf5394ca40a73b42e0dbd450ea5964d2e8
Abu1234 <37133919+Abu1234@users.noreply.github.com>
2021-11-26 17:24:18 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { Routes, RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
-import {MaterialExampleModule} from '../material.module';
+import { MaterialExampleModule } from "../material.module";
+
 import { HomeComponent } from "../home/home.component";
 import { MaterialComponent } from "../material/material.component";
+
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 const ROUTES: Routes = [
   { path: "home", component: HomeComponent },
   { path: "material", component: MaterialComponent },
@@ -17,8 +20,9 @@ const ROUTES: Routes = [
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,MaterialExampleModule,
-BrowserAnimationsModule,
+    FormsModule,
+    MaterialExampleModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(ROUTES)
   ],
   declarations: [AppComponent, HomeComponent, MaterialComponent],

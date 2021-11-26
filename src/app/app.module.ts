2086241d66932385676f1d7e475161f5fb585864
Abu1234 <37133919+Abu1234@users.noreply.github.com>
2021-11-26 11:16:21 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-
-import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from '../home/home.component';
-import { MaterialComponent } from '../material/material.component';
+import { HttpClientModule } from "@angular/common/http";
+import { HomeComponent } from "../home/home.component";
+import { MaterialComponent } from "../material/material.component";
 const ROUTES: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'material', component: MaterialComponent },
+  { path: "home", component: HomeComponent },
+  { path: "material", component: MaterialComponent },
 
-  { path: '**', redirectTo: 'home' },
+  { path: "**", redirectTo: "home" }
 ];
 
 @NgModule({
@@ -20,13 +19,9 @@ const ROUTES: Routes = [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(ROUTES),
+    RouterModule.forRoot(ROUTES)
   ],
-  declarations: [
-    AppComponent,
-
-  HomeComponent,MaterialComponent
-  ],
-  bootstrap: [AppComponent],
+  declarations: [AppComponent, HomeComponent, MaterialComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
